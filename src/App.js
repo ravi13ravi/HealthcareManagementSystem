6eaa117c572e64d5bba006936f5eeb70a3c32137
ravi13ravi <rravi1075@gmail.com>
2024-10-26 11:55:37 +0530

    Update App.js
    adding one more route facebook

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
           <Route path="/register" component={Register} />
           <Route path="/dashboard" component={Dashboard} />
           <Route path="/freebook" component = {Home} />
+          <Route path="/facebook" component = {Login} />
         </Switch>
       </div>
     </Router>
