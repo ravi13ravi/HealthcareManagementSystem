009c3a16c2c176ff3668a858693dc27131c34de2
ravi13ravi <rravi1075@gmail.com>
2024-10-28 13:00:16 +0530

    fix code

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,24 +4,23 @@ import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
 
 // Middleware to authenticate dashboard users
-export const isAdminAuthenticated = catchAsyncErrors(
-  async (req, res, next) => {
-    const token = req.cookies.adminToken;
-    if (!token) {
-      return next(
-        new ErrorHandler("Dashboard User is not authenticated!", 400)
-      );
-    }
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    req.user = await User.findById(decoded.id);
-    if (req.user.role !== "Admin") {
-      return next(
-        new ErrorHandler(`${req.user.role} not authorized for this resource!`, 403)
-      );
-    }
-    next();
+export const isAdminAuthenticated = catchAsyncErrors(async (req, res, next) => {
+  const token = req.cookies.adminToken;
+  if (!token) {
+    return next(new ErrorHandler("Dashboard User is not authenticated!", 400));
   }
-);
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  req.user = await User.findById(decoded.id);
+  if (req.user.role !== "Admin") {
+    return next(
+      new ErrorHandler(
+        `${req.user.role} not authorized for this resource!`,
+        403
+      )
+    );
+  }
+  next();
+});
 
 // Middleware to authenticate frontend users
 export const isPatientAuthenticated = catchAsyncErrors(
@@ -34,7 +33,10 @@ export const isPatientAuthenticated = catchAsyncErrors(
     req.user = await User.findById(decoded.id);
     if (req.user.role !== "Patient") {
       return next(
-        new ErrorHandler(`${req.user.role} not authorized for this resource!`, 403)
+        new ErrorHandler(
+          `${req.user.role} not authorized for this resource!`,
+          403
+        )
       );
     }
     next();
